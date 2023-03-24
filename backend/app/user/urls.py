5b482bf93592ecc7a7800d9133c12bad5ed22835
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-03-24 15:50:23 -0400

    Added user profile endpoint

--- backend/app/user/urls.py
+++ backend/app/user/urls.py
@@ -6,4 +6,5 @@ app_name = "user"
 urlpatterns = (
     path("", views.UserListView.as_view(), name="list"),
     path("<int:id>/", views.UserDetailView.as_view(), name="detail"),
+    path("profile/", views.UserProfileView.as_view(), name="profile"),
 )
